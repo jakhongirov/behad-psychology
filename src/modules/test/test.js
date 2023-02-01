1d495a5cf3d8290045a96ca1c60cebc5afb8afc1
jakhongirov <johongirovdiyorbek08@gmail.com>
2023-02-01 20:16:00 +0500

    add

--- src/modules/test/test.js
+++ src/modules/test/test.js
@@ -99,7 +99,23 @@ module.exports = {
                 image_url = `https://psychology.behad.uz/public/images/${uploadPhoto.filename}`;
             }
 
-            const addTest = await model.addTest(title, description, category_id, result_1, result_2, result_3, result_4, result_5, result_6, image_url, image_name , point_1)
+            let obj1 = {}
+            let obj2 = {}
+            let obj3 = {}
+            let obj4 = {}
+            let obj5 = {}
+            let obj6 = {}
+    
+            obj1[point_1.value] = result_1.value
+            obj2[point_2.value] = result_2.value
+            obj3[point_3.value] = result_3.value
+            obj4[point_4.value] = result_4.value
+            obj5[point_5.value] = result_5.value
+            obj6[point_6.value] = result_6.value
+
+            console.log(obj1);
+
+            const addTest = await model.addTest(title, description, category_id, result_1, result_2, result_3, result_4, result_5, result_6, image_url, image_name)
 
             if (addTest) {
                 return res.json({
